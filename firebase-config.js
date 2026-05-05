219cd50cb68d6df5f935cee66086dc21f442735e
DevCore <drmuhammed204@gmail.com>
2026-05-05 19:20:16 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     authDomain: "devcore-41f88.firebaseapp.com",
     databaseURL: "https://devcore-41f88-default-rtdb.firebaseio.com",
     projectId: "devcore-41f88",
-    storageBucket: "devcore-41f88.appspot.com",
+    storageBucket: "devcore-41f88.firebasestorage.app",
     messagingSenderId: "225328867600",
     appId: "1:225328867600:web:11f248195699641ba59d95",
     measurementId: "G-QEX2DL4E0S"
